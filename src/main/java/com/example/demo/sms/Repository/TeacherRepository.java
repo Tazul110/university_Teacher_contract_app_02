4f4f5cd1727da02b6c416d2f3f6825ed43eb1066
Tazul islam <it49tazulislam@gmail.com>
2023-12-24 11:18:20 +0600

    first commit

---
+++ src/main/java/com/example/demo/sms/Repository/TeacherRepository.java
@@ -0,0 +1,21 @@
+package com.example.demo.sms.Repository;
+import java.util.List;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+
+import com.example.demo.sms.Model.Teacher;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<Teacher,String>{
+
+
+	public List<Teacher> findBytName(String name);
+	public List<Teacher> findBytDept(String dept);
+
+	
+}
